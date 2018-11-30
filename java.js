e36ff6e02c7c59d35d1fc6195e0ffbff67ac2ad2
JohannaBremen <44437662+JohannaBremen@users.noreply.github.com>
2018-11-30 19:38:44 +0200

    Add files via upload
    lehel on lõpuks sisu

--- java.js
+++ java.js
@@ -14,3 +14,32 @@ function tabid(haaaa, nubin) {
     document.getElementById(nubin).style.display = "block";
     haaaa.currentTarget.className += " active";
 }
+
+// Set the date we're counting down to
+var countDownDate = new Date("Dec 24, 2018 00:00:00").getTime();
+
+// Update the count down every 1 second
+var x = setInterval(function() {
+
+  // Get todays date and time
+  var now = new Date().getTime();
+
+  // Find the distance between now and the count down date
+  var distance = countDownDate - now;
+
+  // Time calculations for days, hours, minutes and seconds
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  // Display the result in the element with id="demo"
+  document.getElementById("demo").innerHTML = days + " päeva " + hours + " tundi "
+  + minutes + " minutit " + seconds + " sekundit ";
+
+  // If the count down is finished, write some text 
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("demo").innerHTML = "EXPIRED";
+  }
+}, 1000);
